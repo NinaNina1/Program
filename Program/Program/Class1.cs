9a8d5b28855fe35f15a0cd7c6403c0bc50441b56
filipxa <filipxa@hotmail.com>
2015-05-20 02:04:56 +0200

    Proba1

---
+++ Program/Program/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Gdomacica
+    {
+        public string ime;
+        public string prezime;
+        public string dprezime;
+        public DateTime dtmrodjenja;
+        public long JMBG;
+        public long BrLK;
+        public string Adresa;
+        public string brTel;
+        public string brMobTel;
+        public string email;
+        public string obrazovanje;
+        public string radnoIskustvo;
+        public string hobi;
+        public bulstring alergije, FOgranicenja, ZdrProb, OdbRadi;
+
+    }
+
+    public struct bulstring
+    {
+        public string tekst;
+        public bool bul;
+    }
+
+}
